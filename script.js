f0c8556508fdf1d8e098241729ee6855b6ba2502
inclined4X <jezemiahsam48@gmail.com>
2024-02-04 20:10:14 +0000

    get the day, month and year

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function calculateAge() {
   const today = new Date();
   const birthdateInput = document.getElementById("birthdate").value;
+
+  //  get the day, month, year
+  const birthDay = birthDateParts[0];
+  const birthMonth = birthDateParts[1] - 1;
+  const birthYear = birthDateParts[2];
+
+  //date
+  const birthDateParts = birthdateInput.split("-");
+  //   console.log(birthdateInput);
+  //   console.log(birthDateParts);
 }
 
 const ageCalcuatorForm = document.getElementById("ageCalculator");
