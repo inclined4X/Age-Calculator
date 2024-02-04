dfde08d13d133c4e4a68f7a46d71b6d504464e34
inclined4X <jezemiahsam48@gmail.com>
2024-02-04 21:20:46 +0000

    created a check input and also an if statement to check if the date follows the date format

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 function calculateAge() {
   const today = new Date();
+  console.log(today);
   const birthdateInput = document.getElementById("birthdate").value;
+
+  //date
+  const birthDateParts = birthdateInput.split("-");
 
   //  get the day, month, year
   const birthDay = birthDateParts[0];
   const birthMonth = birthDateParts[1] - 1;
   const birthYear = birthDateParts[2];
 
-  //date
-  const birthDateParts = birthdateInput.split("-");
-  //   console.log(birthdateInput);
-  //   console.log(birthDateParts);
+  const birthDate = new Date(birthYear, birthMonth, birthDay);
+
+  console.log(birthdateInput);
+  console.log(birthDateParts);
+  console.log(birthDay);
+  console.log(birthMonth);
+  console.log(birthYear);
+
+  // check input type
+  const isValidDate = (date) => {
+    return (
+      Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)
+    );
+  };
+
+  if (!isValidDate(birthDate)) {
+    alert(
+      "invalid date format: Please Enter A Valid date in DD-MM-YYYY format"
+    );
+    return;
+  }
 }
 
 const ageCalcuatorForm = document.getElementById("ageCalculator");
